da3524fd2a4354b37b39ee88de900a8407c6ba13
slayerr1 <slayer.r1@gmail.com>
2021-07-02 01:17:05 +0530

    added more js functionality and combined files

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
-let userName = prompt('Please enter you name');
-console.log(userName)
+
+
+function getUserName(){
+    let userName = prompt('Please enter you name');
 document.write('Welcome ' + userName + '!');
+return userName;
+
+}
+
+let userName = getUserName()
+console.log(userName)
+
+
+
+
+function getMuchCash(){
+
+    let muchCash = prompt('Do you have a million $');
+    if(muchCash == 'yes') {
+           document.write('Good to see you again ' + userName + '!');
+    }
+    else if(muchCash == 'no') {
+        
+        alert("Im sorry plese try lings car's insted");
+        window.location.href = "https://www.lingscars.com/links";
+        
+        }
+
+     else {
+        alert("please answer with yes or no");
+        getMuchCash();
+     }   
+
+    
+
+
+         return muchCash
+    }
+    
+    let muchCash = getMuchCAsh();
+    console.log(muchCash)
--- app2.js
+++ app2.js
@@ -1,10 +1,17 @@
-let muchCash = prompt('Do you have a million $');
-if(muchCash == 'yes') {
-       document.write('Good to see you again ' + userName + '!');
-}
-else {
+// function getMuchCash(){
+
+// let muchCash = prompt('Do you have a million $');
+// if(muchCash == 'yes') {
+//        document.write('Good to see you again ' + userName + '!');
+// }
+// else {
     
-    alert("Im sorry plese try lings car's insted");
-    window.location.href = "https://www.lingscars.com/links";
-    // // document.write('Im sorry plese try the amazon store insted')
-}
+//     alert("Im sorry plese try lings car's insted");
+//     window.location.href = "https://www.lingscars.com/links";
+    
+//     }
+
+// }
+
+// let muchCash = getMuchCAsh();
+// console.log(muchCash)
