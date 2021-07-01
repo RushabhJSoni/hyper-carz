ea82134e585526ed24edffbedb6bba2f515da074
slayerr1 <slayer.r1@gmail.com>
2021-07-01 17:07:09 +0530

    added JS to index

---
+++ app.js
@@ -0,0 +1,3 @@
+let userName = prompt('Please enter you name');
+console.log(userName)
+document.write('Welcome ' + userName + '!');
---
+++ app2.js
@@ -0,0 +1,10 @@
+let muchCash = prompt('Do you have a million $');
+if(muchCash == 'yes') {
+       document.write('Good to see you again! ' + userName);
+}
+else {
+    
+    alert("Im sorry plese try lings car's insted");
+    window.location.href = "https://www.lingscars.com/links";
+    // // document.write('Im sorry plese try the amazon store insted')
+}
